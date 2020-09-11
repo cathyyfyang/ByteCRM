4d1d8416ded20c09c2944e64cfaf7cfabb64a5e7
yyr19921203 <123775172@qq.com>
2020-09-11 23:05:11 +1000

    remote changes

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -15,7 +15,9 @@ const Routes = () => (
         <Route exact path="/companies" component={CompanyList} />
         {/* <Route exact path="/logout" component={Logout} />
         <Route exact path="/register" component={RegForm} /> */}
-        {/* others changes */}
+
+        {/* remote changes */}
+
 
     </Switch>
    );

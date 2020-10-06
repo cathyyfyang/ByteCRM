3d4294eaf39496d6d22e1e0445cb25bcba7c8d72
cathy yang <cathy.yf.yang@gmail.com>
2020-10-06 15:11:30 +1000

    updating about contactus website

--- src/ContactUs/ContactUs.js
+++ src/ContactUs/ContactUs.js
@@ -2,25 +2,15 @@ import React from 'react';
 import './ContactUs.scss';
 import Call from '../img/Company/call.svg';
 import Email from '../img/Contact/mail.png';
-import Calltosale from '../img/Contactus/calltosale.jpeg';
-import Emailtous from '../img/Contactus/emailtous.jpeg';
+
 import CatchBackground from './components/CatchBackground';
+import ContactMethod from './components/ContactMethod';
 
 const ContactUs = () => (
     <div className="Contact_container">
         <div className="Contact_layout">
             <CatchBackground />
-            <div className="Contact_method">
-                <div className="Call_sale">
-                    <image className = "Call_background" src = {Calltosale} alt = "Call to sale"/>
-                </div>
-                <div className="Customer_service">
-                    <div className="Email_customer">
-                    <image className = "Email_background" src = {Emailtous} alt = "Call to sale"/>
-                        
-                    </div>
-                </div>
-            </div>
+            <ContactMethod />
         </div>
     </div>
 )
--- src/ContactUs/components/CatchBackground/CatchBackground.jsx
+++ src/ContactUs/components/CatchBackground/CatchBackground.jsx
@@ -4,7 +4,7 @@ import catchBackground from '../../../img/Contactus/getintouch.jpeg';
 
 const CatchBackground = () => (
     <div className="Catch_background">
-    <img className="Catch_img" src={catchBackground} alt="catch us image" />
+    <img className="Catch_us" src={catchBackground} alt="catch us" />
     <div className="Catch_text">
         <h2>Want to get in touch?</h2>
         <p>We are always here and would love to hear from you. Here is the way you can reach us...</p>

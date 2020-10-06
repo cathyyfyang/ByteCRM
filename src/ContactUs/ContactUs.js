dfc3978bf27433696f13b09d7526d563869b7fe4
cathy yang <cathy.yf.yang@gmail.com>
2020-10-06 14:38:20 +1000

    updating of contactus and about websites

--- src/ContactUs/ContactUs.js
+++ src/ContactUs/ContactUs.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import './ContactUs.scss';
-import CatchBackground from '../img/Contactus/getintouch.jpeg'
+import Call from '../img/Company/call.svg';
+import Email from '../img/Contact/mail.png';
+import Calltosale from '../img/Contactus/calltosale.jpeg';
+import Emailtous from '../img/Contactus/emailtous.jpeg';
+import CatchBackground from './components/CatchBackground';
 
 const ContactUs = () => (
     <div className="Contact_container">
         <div className="Contact_layout">
-            <div className="Catch_background">
-                <div className="Catch_text">
-                    <h2>Want to get in touch?</h2>
-                    <p>We are always here and would love to hear from you. Here is the way you can reach us...</p>
+            <CatchBackground />
+            <div className="Contact_method">
+                <div className="Call_sale">
+                    <image className = "Call_background" src = {Calltosale} alt = "Call to sale"/>
                 </div>
+                <div className="Customer_service">
+                    <div className="Email_customer">
+                    <image className = "Email_background" src = {Emailtous} alt = "Call to sale"/>
+                        
+                    </div>
                 </div>
+            </div>
         </div>
-        </div>
+    </div>
 )
 
 export default ContactUs;
